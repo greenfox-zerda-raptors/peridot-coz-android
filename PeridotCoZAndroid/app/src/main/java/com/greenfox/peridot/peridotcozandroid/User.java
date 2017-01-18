71da7f85acbe82b794d4e83ef475074e54a15661
Bedi Szilvia <bedi.szilvia@gmail.com>
2017-01-18 12:59:47 +0100

    user object created, added lombok

---
+++ PeridotCoZAndroid/app/src/main/java/com/greenfox/peridot/peridotcozandroid/User.java
@@ -0,0 +1,15 @@
+package com.greenfox.peridot.peridotcozandroid;
+
+import lombok.Data;
+
+/**
+ * Created by bedij on 2017. 01. 18..
+ */
+@Data
+public class User {
+
+    String username;
+    String password;
+
+
+}
