59a30c9aca710f2a40b1a44268aca39b72e1e103
Mozga Anna <mozgaanna@gmail.com>
2017-01-19 11:26:05 +0100

    onClickListener moved around a couple of times, trying to make is work, testing needed

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/MainActivity.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/MainActivity.java
@@ -1,15 +1,42 @@
 package com.greenfox.peridot.peridot_coz_android.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.greenfox.peridot.peridot_coz_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText loginUsername;
+    EditText loginPassword;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //Save login info
+    public void saveData(View view){
+        SharedPreferences loginData = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = loginData.edit();
+        editor.putString("userName", loginUsername.getText().toString());
+        editor.putString("password", loginPassword.getText().toString());
+        editor.apply();
+
+
+        Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show();
+
+
+
     }
 }
