7415af5481ccbb55369edf2d6537d79e70e88a40
radaladar <bbiczok@gmail.com>
2017-01-23 11:09:03 +0100

    MockCall seems to work, have to wait for activity structure to implement it fully

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/model/api/MockService.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/model/api/MockService.java
@@ -1,16 +1,26 @@
 package com.greenfox.peridot.peridot_coz_android.model.api;
 
+import android.util.Log;
+
 import com.greenfox.peridot.peridot_coz_android.model.pojo.User;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 
 public class MockService implements UserInterface {
+
+    private static final String TAG = "MockService";
 
     public MockService() {
     }
 
     @Override
-    public User getUser() {
-        return null;
+    public Call<User> getUser() {
+        return new MockCall<User>() {
+            @Override
+            public void enqueue(Callback<User> callback) {
+                Log.d(TAG, "Enqueue method called!");
+            }
+        };
     }
 }
