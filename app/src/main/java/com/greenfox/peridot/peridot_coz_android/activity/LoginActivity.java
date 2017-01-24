2285e821f0272f35cf20c26926e9436d6a5f37d0
PappBeata <pappbeata@gmail.com>
2017-01-24 14:35:54 +0100

    Missing parts from AppBar v1

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +28,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         loginUsername = (EditText) findViewById(R.id.loginName);
         loginPassword = (EditText) findViewById(R.id.loginPassword);
@@ -48,6 +54,24 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+ /*       if (id == R.id.menuItem1) {
+            Toast.makeText(this, "Menüeintrag 1 geklickt", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.menuItem2) {
+            Toast.makeText(this, "Menüeintrag 2 geklickt", Toast.LENGTH_SHORT).show();
+            return true;
+        }*/
+        return super.onOptionsItemSelected(item);
     }
 
     public void saveData(View view){
