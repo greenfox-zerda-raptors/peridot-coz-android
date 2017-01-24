7e14fa3d03cd53cf1fb13153a791f04b072df768
PappBeata <pappbeata@gmail.com>
2017-01-24 13:53:09 +0100

    Belongs to prev commit - set AppBar

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/activities/LoginActivity.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/activities/LoginActivity.java
@@ -3,6 +3,9 @@ package com.greenfox.peridot.peridot_coz_android.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +24,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+/*        getSupportActionBar();*/
 
         loginUsername = (EditText) findViewById(R.id.loginName);
         loginPassword = (EditText) findViewById(R.id.loginPassword);
@@ -43,5 +51,22 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+    }
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+ /*       if (id == R.id.menuItem1) {
+            Toast.makeText(this, "Menüeintrag 1 geklickt", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.menuItem2) {
+            Toast.makeText(this, "Menüeintrag 2 geklickt", Toast.LENGTH_SHORT).show();
+            return true;
+        }*/
+        return super.onOptionsItemSelected(item);
     }
 }
