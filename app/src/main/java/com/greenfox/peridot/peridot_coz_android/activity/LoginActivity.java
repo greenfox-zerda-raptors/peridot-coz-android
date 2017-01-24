82b1927e85416ff6eb67a50def4be1d2fd7425a0
Bedi Szilvia <bedi.szilvia@gmail.com>
2017-01-24 17:06:50 +0100

    started testing login button

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     TextView dataView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
--- app/src/test/java/com/greenfox/peridot/peridot_coz_android/ActivityTest.java
+++ app/src/test/java/com/greenfox/peridot/peridot_coz_android/ActivityTest.java
@@ -1,16 +1,24 @@
 package com.greenfox.peridot.peridot_coz_android;
 
+import android.content.Intent;
 import android.os.Build;
-import android.widget.TextView;
-import com.greenfox.peridot.peridot_coz_android.activity.LoggedInActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.greenfox.peridot.peridot_coz_android.activity.LoginActivity;
-import static org.hamcrest.Matchers.equalTo;
+import com.greenfox.peridot.peridot_coz_android.activity.MainActivity;
+import com.greenfox.peridot.peridot_coz_android.model.pojo.User;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import static org.junit.Assert.assertThat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
 
 /**
  * Created by mozgaanna on 20/01/17.
@@ -20,4 +28,55 @@ import static org.junit.Assert.assertThat;
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 public class ActivityTest {
     /// TODO implement test with new activities
+
+    private LoginActivity loginActivity;
+    private Button loginButton;
+    private EditText loginUsername;
+    private EditText loginPassword;
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        loginActivity = Robolectric.setupActivity(LoginActivity.class);
+        loginButton = (Button) loginActivity.findViewById(R.id.loginButton);
+        loginUsername = (EditText) loginActivity.findViewById(R.id.loginName);
+        loginPassword = (EditText) loginActivity.findViewById(R.id.loginPassword);
+    }
+
+    @Test
+    public void testLoginButtonToGoesMainActivity(){
+        loginActivity.findViewById(R.id.loginButton).performClick();
+        Intent expectedIntent = new Intent(loginActivity, MainActivity.class);
+        assertThat(shadowOf(loginActivity).getNextStartedActivity().equals(expectedIntent)
+                );
+    }
+    @Test
+    public void testLoginButtonWithEmptyUsernameOrPasswordStaysOnLoginActivity(){
+        loginActivity.findViewById(R.id.loginButton).performClick();
+        Intent expectedIntent = new Intent(loginActivity, LoginActivity.class);
+        assertThat(shadowOf(loginActivity).getNextStartedActivity().equals(expectedIntent)
+        );
+    }
+
+    @Test
+    public void testLoginButtonWithWrongUsernameOrPasswordStaysOnLoginActivity(){
+        loginUsername.setText("wrongname");
+        loginPassword.setText("wrongpassword");
+        loginActivity.findViewById(R.id.loginButton).performClick();
+        Intent expectedIntent = null;
+        assertThat(shadowOf(loginActivity).getNextStartedActivity().equals(expectedIntent)
+        );
+    }
+
+
+    @Test
+    public void testLoginButtonWithRightPasswordGoesToMainActivity(){
+        loginUsername.setText("aaa");
+        loginPassword.setText("aaa");
+        loginActivity.findViewById(R.id.loginButton).performClick();
+        Intent expectedIntent = new Intent(loginActivity, MainActivity.class);
+        assertThat(shadowOf(loginActivity).getNextStartedActivity().equals(expectedIntent)
+        );
+    }
 }
