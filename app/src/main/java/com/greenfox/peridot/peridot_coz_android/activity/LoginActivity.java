6dfdd4461871ba357e1bd3bade2d9bf60afbae0b
Mozga Anna <mozgaanna@gmail.com>
2017-01-25 11:15:25 +0100

    NavDrawer
    Navigation drawer from the left side is working, need to add fragments
    to buttons

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
@@ -3,6 +3,12 @@ package com.greenfox.peridot.peridot_coz_android.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,7 +22,7 @@ import android.widget.Toast;
 
 import com.greenfox.peridot.peridot_coz_android.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     EditText loginUsername;
     EditText loginPassword;
@@ -54,12 +60,42 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+
+
+
+        //for the nav bar on the left
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, myToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
+
+        //leftnavigation added
+
+        getMenuInflater().inflate(R.menu.left_navigation, menu);
         return super.onCreateOptionsMenu(menu);
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -76,7 +112,11 @@ public class LoginActivity extends AppCompatActivity {
         } else if (id == R.id.logout) {
             Toast.makeText(this, "Itt ki fogunk loggolni vagy nem", Toast.LENGTH_SHORT).show();
             return true;
+        } else if (id == R.id.action_settings) {
+            Toast.makeText(this, "Megnyomtad az Action settingset", Toast.LENGTH_SHORT).show();
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -96,5 +136,15 @@ public class LoginActivity extends AppCompatActivity {
         String pw = loginData.getString("password","");
         String msg = "Saved User Name: " + name + "\nSaved Password: " + pw;
         dataView.setText(msg);
+    }
+
+
+
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
