2e651e84e9f747615b8c91b9863e3a3e483f7d3e
Mozga Anna <mozgaanna@gmail.com>
2017-01-25 12:12:53 +0100

    app title modified

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/LoginActivity.java
@@ -3,15 +3,14 @@ package com.greenfox.peridot.peridot_coz_android.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -65,7 +64,6 @@ public class LoginActivity extends AppCompatActivity implements NavigationView.O
 
 
         //for the nav bar on the left
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, myToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -74,19 +72,13 @@ public class LoginActivity extends AppCompatActivity implements NavigationView.O
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
-
-        //leftnavigation added
-
         getMenuInflater().inflate(R.menu.left_navigation, menu);
         return super.onCreateOptionsMenu(menu);
-
     }
 
     @Override
@@ -143,11 +135,8 @@ public class LoginActivity extends AppCompatActivity implements NavigationView.O
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
         android.app.FragmentManager fragmentManager = getFragmentManager();
-
 
         if (id == R.id.nav_first_layout) {
             fragmentManager.beginTransaction()
