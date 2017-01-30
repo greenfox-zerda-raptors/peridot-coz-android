7835ecd5067045fb2cbb3ed14cc5f07bf43659af
Bedi Szilvia <bedi.szilvia@gmail.com>
2017-01-30 11:05:31 +0100

    LoginRequest done

---
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/model/pojo/LoginRequest.java
@@ -0,0 +1,38 @@
+package com.greenfox.peridot.peridot_coz_android.model.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by bedij on 2017. 01. 30..
+ */
+
+public class LoginRequest {
+
+    @SerializedName("UserName")
+    String username;
+    @SerializedName("Password")
+    String password;
+
+    public LoginRequest() {}
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
