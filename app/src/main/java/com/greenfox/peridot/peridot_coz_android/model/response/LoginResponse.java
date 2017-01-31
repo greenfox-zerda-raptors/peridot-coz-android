ae8286893a03a6fcb1549e1e582738f0167e917b
Mozga Anna <mozgaanna@gmail.com>
2017-01-31 10:00:47 +0100

    response classes done

---
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/model/response/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.greenfox.peridot.peridot_coz_android.model.response;
+
+/**
+ * Created by mozgaanna on 30/01/17.
+ */
+
+public class LoginResponse {
+}
---
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/model/response/UserResponse.java
@@ -0,0 +1,10 @@
+package com.greenfox.peridot.peridot_coz_android.model.response;
+
+
+/**
+ * Created by mozgaanna on 30/01/17.
+ */
+
+public class UserResponse extends Response {
+}
+
