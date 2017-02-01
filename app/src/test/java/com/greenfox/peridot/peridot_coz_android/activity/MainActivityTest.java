90dace75a76c0ffc7c16e383b517f36b5b764235
PappBeata <pappbeata@gmail.com>
2017-02-01 17:54:05 +0100

    Test navigation drawer option items list

--- app/src/test/java/com/greenfox/peridot/peridot_coz_android/activity/MainActivityTest.java
+++ app/src/test/java/com/greenfox/peridot/peridot_coz_android/activity/MainActivityTest.java
@@ -3,8 +3,10 @@ package com.greenfox.peridot.peridot_coz_android.activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.greenfox.peridot.peridot_coz_android.BuildConfig;
@@ -18,6 +20,11 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.internal.ShadowExtractor;
+import org.robolectric.shadows.ShadowToast;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.robolectric.Shadows.shadowOf;
 
 /**
@@ -33,6 +40,7 @@ public class MainActivityTest {
     private SharedPreferences.Editor editor;
     private Toolbar toolbar;
     private DrawerLayout drawer;
+    private NavigationView navigationOptions;
 
     @Before
     public void setUp(){
@@ -52,6 +60,28 @@ public class MainActivityTest {
         Assert.assertNotNull(mainActivity.getSupportActionBar());
     }
 
+    @Test
+    public void testNavDrawerAppearing() throws Exception {
+        Assert.assertNotNull(mainActivity.getDrawerToggleDelegate());
+    }
+
+    @Test
+    public void testNavigationOptionItems() throws Exception {
+        navigationOptions = (NavigationView) mainActivity.findViewById(R.id.nav_view);
+        Assert.assertEquals("Your Kingdom", navigationOptions.getMenu().getItem(0).toString());
+        Assert.assertEquals("Second Layout", navigationOptions.getMenu().getItem(1).toString());
+        Assert.assertEquals("Communicate", navigationOptions.getMenu().getItem(2).toString());
+        Assert.assertEquals("Share", navigationOptions.getMenu().getItem(2).getSubMenu().getItem(0).toString());
+        Assert.assertEquals("Send", navigationOptions.getMenu().getItem(2).getSubMenu().getItem(1).toString());
+    }
+
+/*    @Test
+    public void clickMenuItem_shouldDelegateClickToFragment() {
+        final MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+
+        shadowOf(activity).clickMenuItem(R.id.item4);
+        assertThat(ShadowToast.getTextOfLatestToast()).isEqualTo("Clicked Item 4");
+    }*/
 
     @Test
     public void testIfEmptySharedPrefMainGoesToLogin() throws Exception {
