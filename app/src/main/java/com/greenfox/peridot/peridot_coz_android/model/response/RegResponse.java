997d8c8b3400a805eb8e8cd3164a2798a4e85848
radaladar <bbiczok@gmail.com>
2017-02-01 15:36:07 +0100

    fixed

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/model/response/RegResponse.java
+++
@@ -1,9 +0,0 @@
-package com.greenfox.peridot.peridot_coz_android.model.response;
-
-/**
- * Created by mozgaanna on 30/01/17.
- */
-
-public class RegResponse extends Response{
-    UserResponse user;
-}
