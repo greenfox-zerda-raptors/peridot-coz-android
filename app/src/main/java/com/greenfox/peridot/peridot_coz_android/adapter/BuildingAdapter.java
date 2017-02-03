3eb27cd2aef339f6f7bc54f3201624be7c52300f
Bedi Szilvia <bedi.szilvia@gmail.com>
2017-02-03 14:55:38 +0100

    Position counting corrected

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/adapter/BuildingAdapter.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/adapter/BuildingAdapter.java
@@ -18,9 +18,6 @@ import java.util.ArrayList;
 
 public class BuildingAdapter extends ArrayAdapter<Building> {
 
-    TextView buildingType;
-    TextView buildingLevel;
-    Building building;
     int count = 0;
 
     public BuildingAdapter(Context context, ArrayList<Building> buildings) {
@@ -29,14 +26,13 @@ public class BuildingAdapter extends ArrayAdapter<Building> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
-        building = super.getItem(super.getCount() - position - 1);
+        Building building = super.getItem(position);
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.buildingview, parent, false);
         }
-        buildingType = (TextView) convertView.findViewById(R.id.buildingName);
-        buildingLevel = (TextView) convertView.findViewById(R.id.buildingLevel);
-        buildingType.setText(String.valueOf(building.getType() + " " + count++));
+        TextView buildingType = (TextView) convertView.findViewById(R.id.buildingName);
+        TextView buildingLevel = (TextView) convertView.findViewById(R.id.buildingLevel);
+        buildingType.setText(String.valueOf(building.getType() + " " + (position+1)));
         buildingLevel.setText(String.valueOf("Level " + building.getLevel()));
 
         return convertView;
