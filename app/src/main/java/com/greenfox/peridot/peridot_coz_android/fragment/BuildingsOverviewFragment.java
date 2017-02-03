0737f0aca760520684a9a74103305a9818ee75e1
radaladar <bbiczok@gmail.com>
2017-02-03 16:05:08 +0100

    ultimate demo commit for 02/03 friday

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/fragment/BuildingsOverviewFragment.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/fragment/BuildingsOverviewFragment.java
@@ -9,8 +9,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ListView;
 
 import com.greenfox.peridot.peridot_coz_android.R;
+import com.greenfox.peridot.peridot_coz_android.adapter.BuildingAdapter;
+import com.greenfox.peridot.peridot_coz_android.dagger.DaggerMainActivityComponent;
+import com.greenfox.peridot.peridot_coz_android.model.api.ApiService;
+import com.greenfox.peridot.peridot_coz_android.model.pojo.Building;
+import com.greenfox.peridot.peridot_coz_android.model.response.BuildingsResponse;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by mozgaanna on 25/01/17.
@@ -21,11 +35,32 @@ public class BuildingsOverviewFragment extends Fragment {
     FloatingActionButton mainFab, mineFab, farmFab;
     boolean isMainFabOpen;
     Animation mainFabRotateLeft, mainFabRotateRight, appearSmallFab, disappearSmallFab;
+    public ArrayList<Building> buildings = new ArrayList<>();
+    public ListView listView;
+    public BuildingAdapter adapter;
+    @Inject
+    ApiService apiService;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.buildings_overview_layout, container, false);
+        DaggerMainActivityComponent.builder().build().inject(this);
+        listView = (ListView)contentView.findViewById(R.id.listViewBuilding);
+        adapter = new BuildingAdapter(container.getContext(), buildings);
+        listView.setAdapter(adapter);
+        apiService.getBuildings(1).enqueue(new Callback<BuildingsResponse>() {
+            @Override
+            public void onResponse(Call<BuildingsResponse> call, Response<BuildingsResponse> response) {
+                adapter.clear();
+                adapter.addAll(response.body().getBuildings());
+            }
+            @Override
+            public void onFailure(Call<BuildingsResponse> call, Throwable t) {
+
+            }
+        });
+
         mainFab = (FloatingActionButton) contentView.findViewById(R.id.mainFab);
         mineFab = (FloatingActionButton) contentView.findViewById(R.id.mineFab);
         farmFab = (FloatingActionButton) contentView.findViewById(R.id.farmFab);
