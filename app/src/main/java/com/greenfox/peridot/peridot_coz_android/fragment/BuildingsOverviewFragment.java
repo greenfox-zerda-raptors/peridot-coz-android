547c00459a32473541016f3c9c60092c12bf41f5
Mozga Anna <mozgaanna@gmail.com>
2017-02-06 15:49:18 +0100

    empty lines deleted

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/fragment/BuildingsOverviewFragment.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/fragment/BuildingsOverviewFragment.java
@@ -9,20 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import com.greenfox.peridot.peridot_coz_android.R;
 import com.greenfox.peridot.peridot_coz_android.adapter.BuildingAdapter;
 import com.greenfox.peridot.peridot_coz_android.dagger.DaggerMainActivityComponent;
 import com.greenfox.peridot.peridot_coz_android.model.api.ApiService;
 import com.greenfox.peridot.peridot_coz_android.model.pojo.Building;
 import com.greenfox.peridot.peridot_coz_android.model.response.BuildingsResponse;
-
 import java.util.ArrayList;
-
 import javax.inject.Inject;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -73,8 +68,7 @@ public class BuildingsOverviewFragment extends Fragment {
                 }
             }
         });
-
-
+        
         ListView listView = (ListView) contentView.findViewById(R.id.listViewBuilding);
         adapter = new BuildingAdapter(container.getContext(), buildings);
         listView.setAdapter(adapter);
@@ -92,5 +86,4 @@ public class BuildingsOverviewFragment extends Fragment {
         });
         return contentView;
     }
-
 }
