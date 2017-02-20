1d2bd722054efc954d048746a1ca20515d902136
Mozga Anna <mozgaanna@gmail.com>
2017-02-20 15:31:53 +0100

    AlertDialog set up

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/BaseActivity.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/activity/BaseActivity.java
@@ -1,6 +1,8 @@
 package com.greenfox.peridot.peridot_coz_android.activity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import retrofit2.Response;
 public abstract class BaseActivity extends AppCompatActivity implements Callback {
     ProgressDialog progressDialog;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +33,21 @@ public abstract class BaseActivity extends AppCompatActivity implements Callback
     }
 
     @Override
-    public void onFailure(Call call, Throwable t) {
+    public void onFailure(final Call call, Throwable t) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(BaseActivity.this);
+        alertDialog.setMessage("Do you want to retry?").setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //TODO: call onData or onResponse
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
         progressDialog.dismiss();
         onError(call, t);
     }
