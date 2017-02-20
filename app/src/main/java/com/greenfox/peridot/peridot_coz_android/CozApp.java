f17d30ebc4be2dbe4cd337978db3de552fa1d8fd
PappBeata <pappbeata@gmail.com>
2017-02-20 14:53:39 +0100

    Refactor RestApiManager and take out Application from dagger

--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/CozApp.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/CozApp.java
@@ -2,9 +2,6 @@ package com.greenfox.peridot.peridot_coz_android;
 
 import android.app.Application;
 
-import com.greenfox.peridot.peridot_coz_android.provider.ApplicationProvider;
-import com.greenfox.peridot.peridot_coz_android.provider.DaggerApplicationComponent;
-
 public class CozApp extends Application {
 
     private static Application mApplication;
--- app/src/main/java/com/greenfox/peridot/peridot_coz_android/api/RestApiManager.java
+++ app/src/main/java/com/greenfox/peridot/peridot_coz_android/api/RestApiManager.java
@@ -8,43 +8,29 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RestApiManager {
 
-    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
     private static OkHttpClient client;
-    private static ApiLoginService lApiService;
-    private static ApiService mApiService;
-    private static Retrofit retrofit;
 
-    public static ApiLoginService getLoginApi() {
+    public static OkHttpClient.Builder setHttpClient(){
         DefaultInterceptor defaultInterceptor = new DefaultInterceptor();
-        httpClient.addInterceptor(defaultInterceptor);
-        client = httpClient.build();
-        Retrofit.Builder builder = new Retrofit.Builder()
+        return new OkHttpClient.Builder().addInterceptor(defaultInterceptor);
+    }
+
+    public static Retrofit setRetrofit(OkHttpClient client) {
+        return new Retrofit.Builder()
                 .baseUrl(ApiLoginService.ENDPOINT)
-                .addConverterFactory(GsonConverterFactory.create());
-        retrofit = builder
+                .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
-        if (lApiService == null) {
-            lApiService = retrofit.create(ApiLoginService.class);
-        }
-        return lApiService;
+    }
+
+    public static ApiLoginService getLoginApi() {
+        client = setHttpClient().build();
+        return setRetrofit(client).create(ApiLoginService.class);
     }
 
     public static ApiService getUserApi(final String authToken) {
-        if (!TextUtils.isEmpty(authToken)) {
-            AuthorizationInterceptor authInterceptor = new AuthorizationInterceptor(authToken);
-            DefaultInterceptor defaultInterceptor = new DefaultInterceptor();
-            Retrofit.Builder builder = new Retrofit.Builder()
-                    .baseUrl(ApiService.ENDPOINT)
-                    .addConverterFactory(GsonConverterFactory.create());
-            if (!client.interceptors().contains(authInterceptor)) {
-                httpClient.addInterceptor(defaultInterceptor);
-                httpClient.addInterceptor(authInterceptor);
-                client = httpClient.build();
-                builder.client(client);
-                retrofit = builder.build();
-            }
-        }
-        return retrofit.create(ApiService.class);
+        AuthorizationInterceptor authInterceptor = new AuthorizationInterceptor(authToken);
+        client = setHttpClient().addInterceptor(authInterceptor).build();
+        return setRetrofit(client).create(ApiService.class);
     }
 }
